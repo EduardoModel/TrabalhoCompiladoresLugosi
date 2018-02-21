5f3e9f0b9f7b12eea7839b47e7c75c0a00e4b706
Gustavo Santos <gfdsantos@inf.ufpel.edu.br>
2018-02-21 13:55:57 -0300

    Script de execução alterado

--- rolar.sh
+++ rolar.sh
@@ -1,3 +1,18 @@
-javacc Lugosi.jj
-javac *.java
+#!/bin/bash
+echo "Executando o JAVACC"
+javacc Lugosi.jj > /dev/null
+
+echo "Compilando os arquivos JAVA"
+javac *.java > /dev/null
+
+echo "Executando o Teste 1"
 java Lugosi entrada1.lug
+
+echo "Executando o Teste 2"
+java Lugosi entrada2.lug
+
+echo "Executando o Teste 3"
+java Lugosi entrada3.lug
+
+echo "Limpando repositório"
+rm *.java *.class
